cbe8e140f7bf6f60d5e216fafc15ab0bc9dacf0e
Damien Aicheh <38168153+damienaicheh@users.noreply.github.com>
2024-04-18 09:42:41 +0200

    fix: Dev container.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,4 @@
 #!/bin/sh 
 
-## Grab the latest version of the Ubuntu package repository
-RUN sudo apt-get update 
-
-## Install the curl client
-RUN su vscode -c "sudo apt-get install curl"
-
 # Prepare static web app assets
 npm install -g @azure/static-web-apps-cli
